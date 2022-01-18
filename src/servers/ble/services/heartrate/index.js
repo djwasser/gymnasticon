057adcb0ce39ad852469e3e109bc297f3fcada09
Dan Wasser <djwasser@users.noreply.github.com>
2022-01-17 21:51:41 -0500

    Update index.js

--- src/servers/ble/services/heartrate/index.js
+++ src/servers/ble/services/heartrate/index.js
@@ -1,1 +1,28 @@
+import {PrimaryService} from '@abandonware/bleno';
+import {CyclingPowerMeasurementCharacteristic} from './characteristics/heartrate-measurement';
 
+/**
+ * Bluetooth LE GATT Heartrate Service implementation.
+ */
+export class HeartrateService extends PrimaryService {
+  /**
+   * Create a HeartrateService instance.
+   */
+  constructor() {
+    super({
+      uuid: '180D',
+      characteristics: [
+        new HeartrateMeasurementCharacteristic()
+      ]
+    })
+  }
+
+  /**
+   * Notify subscriber (e.g. Zwift) of new Heartrate Measurement.
+   * @param {object} measurement - new heartrate measurement.
+   * @param {number} measurement.heartrate - current heartrate (bpm)
+   */
+  updateMeasurement(measurement) {
+    this.characteristics[0].updateMeasurement(measurement)
+  }
+}
